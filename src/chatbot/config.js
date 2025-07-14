b7bcf4856c5bc5fe32cee7357f7dc68d055a6075
Phat Gooner <nguyentienphat1604@gmail.com>
2025-07-14 20:28:25 +0700

    update chatbot UI and new favicon

--- src/chatbot/config.js
+++ src/chatbot/config.js
@@ -9,9 +9,6 @@ const config = {
     ],
     botName: "Talkable Assisstant",
     customStyles: {
-        botMessageBox: {
-            backgroundColor: "#00bfff",
-        },
         chatButton: {
             backgroundColor: "#00bfff",
         },
@@ -21,6 +18,13 @@ const config = {
             <img
                 src={logo}
                 alt="Bot Avatar"
+                style={{ width: 40, height: 40, borderRadius: "50%" }}
+            />
+        ),
+        userAvatar: (props) => (
+            <img
+                src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                alt="User Avatar"
                 style={{ width: 40, height: 40, borderRadius: "50%" }}
             />
         ),
